b66c1e0a0d9cf7e379ab1c9a02ab9e462c26727d
Nguyen Viet Son <anhsongoku123@gmail.com>
2023-11-20 12:49:49 +0700

    Giao dien chitiet_sp

---
+++ view/detail_prod.php
@@ -0,0 +1,216 @@
+
+
+        <div class="content">
+            <div class="url">
+                <a href="">Trang chủ / Áo / Áo Polo</a>
+            </div>
+            <div class="info_prod">
+                <div class="box_left">
+                    <div class="anh_to">
+                        <img src="../assets/img/Áo Polo NEWSEVEN Glitch Polo PL.145.jpg" alt="">
+
+                    </div>
+                    <div class="anh_nho">
+                        <img src="../assets/img/Áo Polo NEWSEVEN Glitch Polo PL.145.jpg" alt="">
+                        <img src="../assets/img/Áo Polo NEWSEVEN Glitch Polo PL.145.jpg" alt="">
+                        <img src="../assets/img/Áo Polo NEWSEVEN Glitch Polo PL.145.jpg" alt="">
+                        <img src="../assets/img/Áo Polo NEWSEVEN Glitch Polo PL.145.jpg" alt="">
+                    </div>
+                </div>
+                <div class="box_right">
+                    <div class="name_prod">
+                        <h2>Áo Polo FASHIONSHOP Glitch Polo PL.145</h2>
+                    </div>
+                    <div class="price_prod">
+                        <span class="price_niemyet">300.000đ</span> <span class="price_sale">199.000đ</span>
+                    </div>
+                    <div class="tinh_trang">
+                        <p>Tình trạng: <span>Còn hàng</span></p>
+                    </div>
+                    <div class="ma_sp">
+                        <p>Mã: 1001</p>
+                    </div>
+                    <div class="danh_muc">
+                        <p>Danh mục: Áo Thun</p>
+                    </div>
+                    <div class="tu_khoa">
+                        <p>Từ khóa: Áo, áo polo, polo</p>
+                    </div>
+                    <div class="color_sp">
+                        <div>
+                            <p>Màu: </p>
+                        </div>
+                        <div class="mau">
+                            <ul>
+                                <li> <a class="black" href=""></a></li>
+                                <li> <a class="red" href=""></a></li>
+                                <li> <a class="green" href=""></a></li>
+                                <li><a class="blue" href=""></a></li>
+                            </ul>
+                        </div>
+
+
+
+                    </div>
+                    <div class="size_sp">
+                        <p>Size:</p>
+                        <ul>
+                            <li> <a href="">S</a></li>
+                            <li> <a href="">M</a></li>
+                            <li> <a href="">L</a></li>
+                            <li><a href="">XL</a></li>
+                        </ul>
+                    </div>
+                    <div class="add_cart">
+                        <div>
+
+                            <label for="">Số lượng: </label>
+                            <input type="number" min="1" max="9" name="so_luong" value="1">
+                            <a href="">Thêm vào giỏ hàng</a>
+                        </div>
+                        <div>
+
+                        </div>
+                    </div>
+                    <div class="buy_now">
+                        <a href="">Mua ngay</a>
+                    </div>
+                </div>
+            </div>
+
+
+
+            <div class="row">
+                <div class="title_prod">
+                    <h2>Mô tả sản phẩm</h2>
+                </div>
+                <hr>
+                <div class="description_prod">
+                    <p>Thông tin sản phẩm Áo Polo NEWSEVEN Glitch Polo PL.145: </p>
+                    <p> – Hàng chuẩn NEWSEVEN sản xuất, tem mác chuẩn chính hãng.</p>
+                    <p>
+
+                        – Chất liệu: 100% cotton – định lượng 250GSM. Dày dặn. Đứng form.
+                    </p>
+                    <p>
+                        Đã xử lý chống co”
+                    </p>
+
+                    <p>
+                        – Cổ áo: Cổ dệt dày dặn,đã xử lý chống nhăn, bai dão
+                    </p>
+                </div>
+            </div>
+            <div class="row binhluan">
+                <div class="title_binhluan">
+                    <h3>Bình luận</h3>
+                    <hr>
+                </div>
+
+
+                <div class="box-content">
+                    <table border="0" class="table-binhluan">
+                        <thead>
+                            <tr>
+                                <th>Nội dung</th>
+                                <th>Tên khách hàng</th>
+                                <th>Ngày bình luận</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td>Sản phẩm tốt quá, dùng 3 ngày là rách</td>
+                                <td>Ocean</td>
+                                <td>2023-10-21 10:59:37</td>
+                            </tr>
+                            <tr>
+                                <td>Sản phẩm tốt quá, dùng 3 ngày là rách</td>
+                                <td>Ocean</td>
+                                <td>2023-10-21 10:59:37</td>
+                            </tr>
+                            <tr>
+                                <td>Sản phẩm tốt quá, dùng 3 ngày là rách</td>
+                                <td>Ocean</td>
+                                <td>2023-10-21 10:59:37</td>
+                            </tr>
+                        </tbody>
+                    </table>
+
+                </div>
+                <div>
+                    <form action="">
+                        <input type="text" name="content_bl" placeholder="Nhập bình luận của bạn...">
+                        <button type='submit' name='btnSubmit'>Gửi bình luận</button>
+                    </form>
+
+                </div>
+
+
+
+            </div>
+            <div class='see_more' class="row mt">
+                <h3>Xem thêm sản phẩm khác:</h3>
+                <!-- //Xuất các sanpham cùng loại lên , cùng iddm  -->
+                <ul>
+                    <li>
+                        <div class="img_prod">
+                            <a href="">
+                                <img width="100px" src="../assets/img/Áo Polo NEWSEVEN Glitch Polo PL.145.jpg" alt="">
+                            </a>
+                        </div>
+                        <div class="name_prod">
+                            <a href=""> Áo Polo NEWSEVEN Glitch Polo</a>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="img_prod">
+                            <a href="">
+                                <img width="100px" src="../assets/img/Áo Polo NEWSEVEN Glitch Polo PL.145.jpg" alt="">
+                            </a>
+                        </div>
+                        <div class="name_prod">
+                            <a href=""> Áo Polo NEWSEVEN Glitch Polo</a>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="img_prod">
+                            <a href="">
+                                <img width="100px" src="../assets/img/Áo Polo NEWSEVEN Glitch Polo PL.145.jpg" alt="">
+                            </a>
+                        </div>
+                        <div class="name_prod">
+                            <a href=""> Áo Polo NEWSEVEN Glitch Polo</a>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="img_prod">
+                            <a href="">
+                                <img width="100px" src="../assets/img/Áo Polo NEWSEVEN Glitch Polo PL.145.jpg" alt="">
+                            </a>
+                        </div>
+                        <div class="name_prod">
+                            <a href=""> Áo Polo NEWSEVEN Glitch Polo</a>
+                        </div>
+                    </li>
+                    <li>
+                        <div class="img_prod">
+                            <a href="">
+                                <img width="100px" src="../assets/img/Áo Polo NEWSEVEN Glitch Polo PL.145.jpg" alt="">
+                            </a>
+                        </div>
+                        <div class="name_prod">
+                            <a href=""> Áo Polo NEWSEVEN Glitch Polo</a>
+                        </div>
+                    </li>
+
+
+
+                </ul>
+
+
+
+
+            </div>
+        </div>
+    </div>
+</body>
---
+++ view/index.php
@@ -0,0 +1,191 @@
+<?php
+session_start();
+include "../model/pdo.php";
+include "../model/danhmuc.php";
+include "../model/sanpham.php";
+include "../model/khachhang.php";
+include "../model/taikhoan.php";
+include "../model/binhluan.php";
+?>
+
+<?php
+include "_header.php";
+
+
+?>
+ <?php
+    //Các biến dùng chung
+    $dssp = danhsach_sanpham();
+    $dsdm = danhsach_danhmuc();
+    $top10 = top10_sanpham();
+
+    if (!isset($_SESSION['myCart'])) {
+        $_SESSION['myCart'] = [];
+    }
+    //Controller
+    if (isset($_GET['act']) && $_GET['act'] != "") {
+        $act = $_GET['act'];
+        switch ($act) {
+            case 'home': {
+                    include "home.php";
+                    break;
+                }
+
+            case 'timdm': {
+                    if (isset($_GET['iddm']) && $_GET['iddm'] > 0) {
+                        $dssp = tim_sanpham_theodm($_GET['iddm']);
+                    }
+                    include "home.php";
+                    break;
+                }
+
+            case "chitietsp": {
+                    if (isset($_GET['idsp']) && $_GET['idsp'] > 0) {
+                        $dsbl = load_binhluan($_GET['idsp']);
+                        $sanpham = one_sanpham($_GET['idsp']);
+                        $sanpham_lq = xemthem_sanpham($_GET['idsp']);
+                        tangluotxem($_GET['idsp']);
+                    }
+                    if (isset($_POST['btnSubmit'])) {
+                        add_binhluan($_POST['noidung'], $_POST['iduser'], $_POST['idpro'], $_POST['datetime']);
+                        header("Location: ?act=chitietsp&idsp=" . $_GET['idsp']);
+                    }
+                    include "detail_prod.php";
+                    break;
+                }
+            case 'timkiem': {
+                    if (isset($_POST['timkiem'])) {
+                        $keyword = $_POST['keyword'];
+                        $dssp = tim_kiem_san_pham($keyword);
+                    }
+
+                    include "home.php";
+                    break;
+                }
+
+            case "register": {
+                    if (isset($_POST['btnSubmit'])) {
+                        add_khachhang($_POST['tenkh'], $_POST['pass'], $_POST['email'], $_POST['address'], $_POST['tel'], $_POST['role']);
+                        $thongbao = "Đăng ký thành công.";
+                    }
+                    include "register.php";
+                    break;
+                }
+
+            case "login": {
+                    if (isset($_POST['btnSubmit'])) {
+                        if ($_POST['user'] != "" && $_POST['pass'] != "") {
+                            $check_user = check_user($_POST['user'], $_POST['pass']);
+                            if (is_array($check_user)) {
+                                //Tạo ra biến Session để lưu kết quả vừa tìm đc
+                                $_SESSION['user'] = $check_user;
+
+                                header("location:?act=login");
+                            } else {
+                                $thongbao = "Tài khoản không tồn tại. Vui lòng kiểm tra hoặc đăng ký";
+                            }
+                        } else {
+                            $thongbao = "Tên đăng nhập và mật khẩu không được để trống";
+                        }
+                    }
+                    include "home.php";
+                    break;
+                }
+            case "edit_tk": {
+                    if (isset($_POST['btnSubmit'])) {
+                        edit_tk($_POST['id'], $_POST['user'], $_POST['pass'], $_POST['email'], $_POST['address'], $_POST['tel']);
+                        $_SESSION['user'] = check_user($_POST['user'], $_POST['pass']);
+
+                        $thongbao = "Cập nhật tài khoản thành công!";
+                    }
+
+                    include "edit_tk.php";
+                    break;
+                }
+
+            case "logout": {
+                    session_unset(); //Xóa hết tất cả ss
+                    header("Location:?act=index");
+                    break;
+                }
+
+
+            case 'fg_password': {
+                    if (isset($_POST['btnSubmit'])) {
+                        if ($_POST['email'] != "") {
+                            $check_email = check_email($_POST['email']);
+                            if (is_array($check_email)) {
+                                $thongbaomk = "Mật khẩu của bạn là:" . $check_email['pass'];
+                            } else {
+                                $thongbaomk = "Email này không tồn tại!";
+                            }
+                        }else {
+                            $thongbaomk = "Email không được để trống";
+                        }
+                    }
+                    include "forgot_password.php";
+                    break;
+                }
+            case "add_to_cart": {
+                    if (isset($_POST['btnSubmit'])) {
+                        $id = $_POST['id'];
+                        $name = $_POST['name'];
+                        $img = $_POST['img'];
+                        $price = floatval($_POST['price']);
+                        $soluong = intval(1);
+                        $ttien = ($soluong * $price);
+
+                        $add_sp = [$id, $name, $img, $price, $soluong, $ttien];
+                        array_push($_SESSION['myCart'], $add_sp);
+                    }
+                    include "cart/cart.php";
+                    break;
+                }
+            case "deletespcart": {
+                    if (isset($_GET['idcart'])) {
+                        $idcart = $_GET['idcart'];
+                        array_splice($_SESSION['myCart'], $idcart, 1);
+                    } else {
+                        $_SESSION['myCart'] = [];
+                    }
+                }
+                header("location:?act=cart");
+
+                break;
+            case 'cart': {
+                    include "cart/cart.php";
+                    break;
+                }
+            case "bill": {
+                    include "cart/bill.php";
+                    break;
+                }
+            case "bill_confirm": {
+                    if (isset($_POST['btnSubmit'])) {
+                        $name = $_POST['user'];
+                        $email = $_POST['email'];
+                        $address = $_POST['address'];
+                        $tel = $_POST['tel'];
+                        $ngayDatHang = date('Y-m-d H:i:s', strtotime('+5 hours'));
+
+                        $_SESSION['myCart'] = [];
+                    }
+                    include "cart/bill_confirm.php";
+                    break;
+                }
+            default: {
+                    include "home.php";
+                    break;
+                }
+        }
+    } else {
+        include "home.php";
+    }
+    ?>
+
+
+
+<?php
+include "_footer.php";
+?>
+    
