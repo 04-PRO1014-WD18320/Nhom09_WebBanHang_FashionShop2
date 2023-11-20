53afb6bd96cdbee83e112eb171138fc36707bf7d
Ocean <vthadaiduong@gmail.com>
2023-11-20 11:24:36 +0700

    danhmuc+sanpham

--- admin/index.php
+++ admin/index.php
@@ -1,15 +1,15 @@
 <?php
+    session_start();
 
-
-include "../model/pdo.php";
-include "../model/danhmuc.php";
-include "../model/sanpham.php";
-include "../model/khachhang.php";
-include "../model/binhluan.php";
-include "../model/thongke.php";
-include 'header.php';
-include 'boxleft.php';
-include 'menu.php';
+    include "../model/pdo.php";
+    include "../model/danhmuc.php";
+    include "../model/sanpham.php";
+    include "../model/khachhang.php";
+    include "../model/binhluan.php";
+    include "../model/thongke.php";
+    include 'header.php';
+    include 'boxleft.php';
+    include 'menu.php';
 
 ?>
 
@@ -58,16 +58,107 @@ include 'menu.php';
                 break;
 
             case 'dssanpham':
-
+                if (isset($_POST['clickOK']) && ($_POST['clickOK'])) {
+                    $keyw = $_POST['keyw'];
+                    $iddm = $_POST['iddm'];
+                } else {
+                    $keyw = "";
+                    $iddm = 0;
+                }
+                $list_dm = danhsach_danhmuc();
+                $list_sp = loadall_sanpham($keyw, $iddm);
                 include "sanpham/dssanpham.php";
+                break;
+            case 'add_sp':
+                $ses= (isset($_SESSION['inputCounter'])) ? $_SESSION['inputCounter'] : 1;
+                echo 'ses trong file index: '.$ses;
+                $list_dm = danhsach_danhmuc();
+                $list_ms = danhsach_mausac();
+                $list_kt = danhsach_kichthuoc();
+                if(isset($_POST['submit'])){
+                    $name = $_POST['name'];
+                    $price_niemyet = $_POST['price_niemyet'];
+                    $price_sale = $_POST['price_sale'];
+                    $mota = $_POST['mota'];
+                    $iddm = $_POST['iddm'];
+                    $idsp = 0;
+                    $img_phu = '';
+        
+                    $idms_ = '';
+                    $idkt_ = '';
+                    $soluong_ = array();
+                    
+        
+                    $img_main = $_FILES['image_main']['name'];
+                                           echo $img_main;
+                    $target_dir = "../upload/";
+                    $target_file = $target_dir . basename($_FILES['image_main']['name']);
+                    //                    echo $target_file;
+                    if (move_uploaded_file($_FILES['image_main']['tmp_name'], $target_file)) {
+                        echo "Bạn đã upload ảnh thành công";
+                    } else {
+                        echo "Upload ảnh không thành công";
+                    }
+                    // thêm bảng sp
+                    insert_sanpham($name, $img_main, $price_niemyet, $price_sale, $mota, $iddm);
+                    $sp_insert = load_sp_insert();
+                    foreach($sp_insert as $sp ){
+                        extract($sp);
+                        $idsp = $id;
+                    }
+                    echo 'id -san pham vua nhap la: '.$idsp;
+                    //them bang hinhanh
+                    $img = $_FILES['image'];
+                    $imgCount = count($img['name']);
+        
+                    $target_dir_arr = "../upload/";
+                    for($i = 0; $i < $imgCount; $i++){
+                        echo '<br>'.$img['name'][$i].'<br>';
+                        $img_phu= $img['name'][$i];
+
+                        insert_hinhanh($img_phu, $idsp);
+
+                        $target_file_arr = $target_dir_arr . basename($img['name'][$i]);
+                        // echo $target_file_arr.'<br>';
+                        if (move_uploaded_file($img['tmp_name'][$i], $target_file_arr)) {
+                                echo "Bạn đã upload ảnh thành công";
+                        } else {
+                                echo "Upload ảnh không thành công";
+                        }
+        
+                    }
+                    //thêm bảng biến thể
+                    for ($j = 0; $j <= $ses; $j++) {
+                        $idms_[$j] = $_POST['idms_' . $j];
+                        $idkt_[$j] = $_POST['idkt_' . $j];
+                        $soluong_[$j] = $_POST['soluong_' . $j];
+                        var_dump($soluong_[$j]);
+                        // echo $soluong_[$j];
+                        // echo '<br>id_mau: '.$idms_[$j];
+                        // echo '<br>id_kich thuoc: '.$idkt_[$j];
+                        // echo '<br>soluong: '.$soluong_[$j];
+                        insert_bien_the($idsp, $idms_[$j], $idkt_[$j], $soluong_[$j]);
+        
+                    }
+        
+        
+                
+                    // insert_sanpham($name, $img_main, $price_niemyet, $price_sale, $mota, $iddm);
+                    // $sp_insert = load_sp_insert();
+                    // foreach($sp_insert as $sp ){
+                    //     extract($sp);
+                    //     $idsp = $id;
+                    // }
+                    // echo 'id -san pham vua nhap la: '.$idsp;
+                    // insert_hinhanh($img_phu, $idsp);
+                }
+                
+                // $list_sp = loadall_sanpham($keyw, $iddm);
+                include "sanpham/addsanpham.php";
                 break;
             case 'editsanpham':
 
                 include "sanpham/editsanpham.php";
-                break;
-            case 'addsanpham':
-
-                include "sanpham/addsanpham.php";
                 break;
             case 'dsdonhang':
 
@@ -79,13 +170,12 @@ include 'menu.php';
                 break;
             case "deletebl":
                 if (isset($_GET['idbl'])) {
-              
+
                     delete_binhluan($_GET['idbl']);
                     $dsbl = danhsach_binhluan();
-                    
                 }
                 // header("location: ?act=home");
-                
+
                 include "binhluan/dsbinhluan.php";
                 break;
 
@@ -144,7 +234,18 @@ include 'menu.php';
 
                 break;
 
-
+            case 'test_loadall_sp': 
+                if (isset($_POST['clickOK']) && ($_POST['clickOK'])) {
+                    $keyw = $_POST['keyw'];
+                    $iddm = $_POST['iddm'];
+                } else {
+                    $keyw = "";
+                    $iddm = 0;
+                }
+                $list_dm = danhsach_danhmuc();
+                $list_sp = loadall_sanpham($keyw, $iddm);
+                include "test_render/dssanpham.php";
+                break;
             default:
                 include "../404.php";
                 break;
--- model/sanpham.php
+++ model/sanpham.php
@@ -7,6 +7,69 @@ function danhsach_sanpham()
     $result = pdo_query($sql);
     return $result;
 }
+function loadall_sanpham($keyw = "", $iddm = 0)
+{
+    $sql = "SELECT sanpham.id,
+                   sanpham.name,
+                   sanpham.img,
+                   sanpham.price_niemyet,
+                   sanpham.price_sale,
+                   bien_the.so_luong,
+                   mau_sac.value AS mau_sac,
+                   kich_thuoc.value AS kich_thuoc,
+                   sanpham.luotxem, 
+                   sanpham.iddm
+            FROM sanpham
+            JOIN bien_the ON sanpham.id = bien_the.id_sp
+            JOIN mau_sac ON bien_the.id_mau_sac = mau_sac.id
+            JOIN kich_thuoc ON bien_the.id_kich_thuoc = kich_thuoc.id
+            -- ORDER BY sanpham.id 
+            WHERE 1";
+    // where 1 tức là nó đúng
+    if ($keyw != "") {
+        $sql .= " AND sanpham.name like '%" . $keyw . "%'";
+    }
+    if ($iddm > 0) {
+        $sql .= " AND sanpham.iddm ='" . $iddm . "'";
+    }
+    $sql .= " ORDER BY sanpham.id";
+    $listsanpham = pdo_query($sql);
+    return  $listsanpham;
+}
+function load_sp_insert(){
+    $sql = "SELECT * FROM sanpham ORDER BY id DESC LIMIT 0,1";
+    $result = pdo_query($sql);
+    return $result;
+}
+function danhsach_mausac()
+{
+    $sql = "SELECT * FROM mau_sac ";
+
+    $result = pdo_query($sql);
+    return $result;
+}
+function danhsach_kichthuoc()
+{
+    $sql = "SELECT * FROM kich_thuoc ";
+
+    $result = pdo_query($sql);
+    return $result;
+}
+function insert_sanpham($name, $img_main, $price_niemyet, $price_sale, $mota, $iddm){
+    $sql = "INSERT INTO sanpham (name,img, price_niemyet, price_sale, mota, iddm)
+    VALUES ('$name','$img_main','$price_niemyet', '$price_sale', '$mota', '$iddm');";
+    pdo_execute($sql);
+}
+function insert_bien_the($idsp, $idms, $idkt, $soluong){
+    $sql = "INSERT INTO bien_the (id_sp,id_mau_sac, id_kich_thuoc, so_luong)
+    VALUES ('$idsp','$idms','$idkt', '$soluong');";
+    pdo_execute($sql);
+}
+function insert_hinhanh($url, $idsp){
+    $sql = "INSERT INTO hinhanh (url, id_sp)
+    VALUES ('$url','$idsp');";
+    pdo_execute($sql);
+}
 function one_sanpham($idsp)
 {
     $sql = "SELECT * FROM sanpham where id= $idsp";
